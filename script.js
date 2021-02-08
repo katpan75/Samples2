0d225ccb11aa703409d51317947a7103999ec668
katpan75 <kpan@risd.edu>
2021-02-08 00:49:42 -0500

    first draft of website

--- script.js
+++ script.js
@@ -1,25 +1,61 @@
 $(function(){
-    $(".words.knittingone").hover(function(){
-    console.log("hovered")
-    let x = 0;
 
-    if(x == 0){
-        $(".words.knittingone").click(function() {
-            $(".words.knittingone").css("visibility","hidden");
-            x++;
-            console.log(x);
-         });
-        }else if (x==1){
-            $(".words.knittingone").click(function() {
-                $(".words.knittingone").css("visibility","visible");
-                x--;
-                console.log(x);
-            });
-            
-        }
-
-    });
-        
+$(".button.aa").click(function(){
+    $(".position.A").animate({width: 'toggle'});
+});
+$(".button.bb").click(function(){
+    $(".position.B").animate({width: 'toggle'});
+});
+$(".button.cc").click(function(){
+    $(".position.C").animate({width: 'toggle'});
+});
+$(".button.dd").click(function(){
+    $(".position.D").slideToggle();
+    
+});
+$(".button.ee").click(function(){
+    $(".position.E").slideToggle();
+    
 });
 
-//$(".words.knittingone").css("transform","translate:(-100vw);");
+$(".button.aa").click(function(){
+    $(".position.B h2").toggle();
+    $(".position.B p").html("<h1>Machine Knitting Samples</h1>").toggle();
+
+    $(".position.C h2").toggle();
+    $(".position.C p").html("Fall 2018, all samples done on a single bed knitting machine.").toggle();
+
+});
+
+$(".button.bb").click(function(){
+    $(".position.A h2").toggle();
+    $(".position.A p").html("<h1>Hand Woven Samples</h1>").toggle();
+
+    $(".position.C h2").toggle();
+    $(".position.C p").html("Spring 2019, all samples done on a 8-harness hand loom.").toggle();
+
+});
+
+$(".button.cc").click(function(){
+    $(".position.B h2").toggle();
+    $(".position.B p").html("Fall 2019, all samples done on 24-harness dobby loom.").toggle();
+
+    $(".position.A h2").toggle();
+    $(".position.A p").html("<h1>Dobby Woven Samples </h1>").toggle();
+
+});
+
+$(".button.dd").click(function(){
+    $(".position.C h2").toggle();
+    $(".position.C p").html("<h1>Sculptural Stocking</h1>").toggle();
+
+    $(".position.E h2").toggle();
+    $(".position.E p").html("Fall 2018, inspired by the scales on reptiles, the fabric is made using monofilament, thin cotton and metallic thread to create a second skin. Alternating picot hems are used to slice up moments of exposure down the leg of the wearer. In obscuring the hips, knees and feet on the body, the stocking transforms the traditional silhouette.").toggle();
+});
+
+$(".button.ee").click(function(){
+    $(".position.D h2").toggle();
+    $(".position.D p").html("<h1>Project: Day in the Life of, a Study of Blocking</h1>Spring 2019, using one fixed design screen and one blank screen. The prints focus on color choice and blocking techniques.").toggle();
+});
+
+});
